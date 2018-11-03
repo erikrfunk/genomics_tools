ba96fa5433567619543a09c04ce65d4d31ecc6a4
erikrfunk <erik.funk@colorado.edu>
2018-11-03 11:21:32 -0600

    added tests

---
+++ tests/test_code.sh
@@ -0,0 +1,30 @@
+This is the first line of "Code"
+
+commands here
+
+more commands here
+
+This is the last line of "Code"
+#!/bin/bash#!/bin/bashThis is the first line of "Code"
+
+commands here
+
+more commands here
+
+This is the last line of "Code"
+#!/bin/bash#!/bin/bash
+This is the first line of "Code"
+
+commands here
+
+more commands here
+
+This is the last line of "Code"
+#!/bin/bash#!/bin/bashThis is the first line of "Code"
+
+commands here
+
+more commands here
+
+This is the last line of "Code"
+#!/bin/bash
