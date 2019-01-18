bbab2a641a5b4256eed7b91153223e0234c60860
Erik R. Funk <erikrfunk@gmail.com>
2019-01-18 14:09:01 -0700

    first commit

---
+++ pervariant_hetero_count.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+'''
+Parses a VCF file to tally the total number of heterozygotes per variant
+Only argument is a vcf file
+
+imports from VCFparser'''
+
+import sys
+from VCFparser import Variants
+
+#Parameters
+vcfPath = sys.argv[1] #vcf file to parse
+
+#Format genotypes to count
+print("Formatting genotypes")
+vcf = Variants(vcfPath)
+genotypeLines = vcf.genotype_convert(genoformat = 'numeric')
+
+#Tally will create a dict where the key is the variant number and value is the
+#proportion of individuals genotyped at that locus that are heterozygous
+print("Tallying heterozygotes per variant")
+heteroCount = {}
+variantCount = 0
+
+for variant in genotypeLines:
+    variantCount += 1
+    numGenotypedSamples = 0
+    numHeterozygotes = 0
+    allGenotypes = variant[9:] #Cut out the position and ref info
+    for genotype in allGenotypes:
+        alleles = genotype.split(sep = '/')
+        if alleles[0] == alleles[1] and '.' not in alleles:
+            numGenotypedSamples += 1
+        elif alleles[0] != alleles[1] and '.' not in alleles:
+            numGenotypedSamples += 1
+            numHeterozygotes += 1
+    heteroCount[variantCount] = numHeterozygotes / numGenotypedSamples
+
+with open('Per-variant_Hetero_Counts.txt', 'w') as o:
+    for i in range(1, variantCount+1):
+        o.write("%s\t%s\n" % (str(i), str(heteroCount[i])))
